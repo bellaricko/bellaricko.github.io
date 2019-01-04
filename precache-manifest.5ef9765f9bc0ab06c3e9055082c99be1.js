4828109305fd1e532a774b4a0a41c6f02b30c68c
Izabella Lönnqvist <izabellalonnqvist@Izabellas-MBP.lan>
2019-01-04 09:20:11 +0100

    Updates

---
+++ precache-manifest.5ef9765f9bc0ab06c3e9055082c99be1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "edeff788bfc50d2b5772",
+    "url": "/static/css/main.2d8fdc95.chunk.css"
+  },
+  {
+    "revision": "edeff788bfc50d2b5772",
+    "url": "/static/js/main.edeff788.chunk.js"
+  },
+  {
+    "revision": "fcbf51108b7c857e677c",
+    "url": "/static/js/1.fcbf5110.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e784982c56670819647da5d4b7a55b81",
+    "url": "/index.html"
+  }
+];
